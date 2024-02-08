13bd3acbb8fc40eca944cd6df6f7e38445616998
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-02-08 16:20:29 -0300

    feat: add loading component to layout as fallback

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -7,6 +7,7 @@ import {
   Inter_600SemiBold,
   Inter_700Bold,
 } from '@expo-google-fonts/inter';
+import { Loading } from '@/components/loading';
 
 export default function Layout() {
   const [fontsLoaded] = useFonts({
@@ -17,7 +18,7 @@ export default function Layout() {
   });
 
   if (!fontsLoaded) {
-    return;
+    <Loading />;
   }
 
   return (
