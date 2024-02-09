ae5e171bc1b4225a4e17bbfaf468052a9639dfc1
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-02-09 08:46:43 -0300

    feat: create category-button component

---
+++ src/components/category-button.tsx
@@ -0,0 +1,25 @@
+import { Text, Pressable, PressableProps } from 'react-native';
+import { clsx } from 'clsx';
+
+type CategoryButtonProps = PressableProps & {
+  title: string;
+  isSelected?: boolean;
+};
+
+export default function CategoryButton({
+  title,
+  isSelected,
+  ...rest
+}: CategoryButtonProps) {
+  return (
+    <Pressable
+      className={clsx(
+        'bg-slate-800 px-4 justify-center rounded-md h-10',
+        isSelected && 'border-2 border-lime-300'
+      )}
+      {...rest}
+    >
+      <Text className='text-slate-100 font-subtitle text-sm'>{title}</Text>
+    </Pressable>
+  );
+}
