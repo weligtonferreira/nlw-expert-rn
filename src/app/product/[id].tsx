bfd60fb7ae9d590e92663e71f64a527049da6b0c
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-02-11 10:30:48 -0300

    feat: create product page

---
+++ src/app/product/[id].tsx
@@ -0,0 +1,59 @@
+import { Image, Text, View } from 'react-native';
+import { useLocalSearchParams } from 'expo-router';
+import { Feather } from '@expo/vector-icons';
+
+import { formatCurrency } from '@/utils/functions/format-currency';
+import { PRODUCTS } from '@/utils/data/products';
+
+import { Button } from '@/components/button';
+import { LinkButton } from '@/components/link-button';
+
+export default function Product() {
+  const { id } = useLocalSearchParams();
+
+  const product = PRODUCTS.filter((item) => item.id === id)[0];
+
+  return (
+    <View className='flex-1'>
+      <Image
+        source={product.cover}
+        className='w-full h-52'
+        resizeMode='cover'
+      />
+
+      <View className='p-5 mt-8 flex-1'>
+        <Text className='text-white text-2xl font-bold my-2'>
+          {product.title}
+        </Text>
+
+        <Text className='text-lime-400 text-3xl font-heading my-2'>
+          {formatCurrency(product.price)}
+        </Text>
+
+        <Text className='text-slate-400 font-body text-base leading-6 mb-6'>
+          {product.description}
+        </Text>
+
+        {product.ingredients.map((ingredient) => (
+          <Text
+            key={ingredient}
+            className='text-slate-400 font-body text-base leading-6'
+          >
+            {'\u2022'} {ingredient}
+          </Text>
+        ))}
+      </View>
+
+      <View className='p-5 pb-8 gap-5'>
+        <Button>
+          <Button.Icon>
+            <Feather name='plus-circle' size={20} />
+          </Button.Icon>
+          <Button.Text>Adicionar ao pedido</Button.Text>
+        </Button>
+
+        <LinkButton title='Voltar ao cardápio' href='/' />
+      </View>
+    </View>
+  );
+}
