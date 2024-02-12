1bae06589f4f814479a1dff6fce19a9fdfc2c10c
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-02-12 10:20:56 -0300

    feat: add type to section list

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import { useState, useRef } from 'react';
 import { View, FlatList, SectionList, Text } from 'react-native';
 
-import { CATEGORIES, MENU } from '@/utils/data/products';
+import { CATEGORIES, MENU, ProductProps } from '@/utils/data/products';
 
 import { Header } from '@/components/header';
 import { Product } from '@/components/product';
@@ -13,7 +13,7 @@ export default function Home() {
   const cartStore = useCartStore();
   const [category, setCategory] = useState(CATEGORIES[0]);
 
-  const sectionListRef = useRef<SectionList>(null);
+  const sectionListRef = useRef<SectionList<ProductProps>>(null);
 
   const cartQuantityItems = cartStore.products.reduce(
     (total, product) => total + product.quantity,
