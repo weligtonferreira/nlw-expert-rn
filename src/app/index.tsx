e1f6767b1ecbccfb68bed65891e84279ed83267e
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-02-12 16:05:10 -0300

    fix: change main view style

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -37,7 +37,7 @@ export default function Home() {
   }
 
   return (
-    <View className='flex-1 pt-10'>
+    <View className='flex-1 pt-8'>
       <Header title='Faça seu pedido' cartQuantityItems={cartQuantityItems} />
 
       <FlatList
