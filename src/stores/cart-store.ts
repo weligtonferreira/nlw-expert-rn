20c9cd36e60a0be016ee155eb5f516bc517ee1c3
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-02-12 18:24:38 -0300

    feat: add the clean cart function

--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -14,6 +14,7 @@ type StateProps = {
   products: ProductCartProps[];
   add: (product: ProductProps) => void;
   remove: (productId: string) => void;
+  clear: () => void;
 };
 
 export const useCartStore = create(
@@ -30,6 +31,8 @@ export const useCartStore = create(
         set((state) => ({
           products: cartInMemory.remove(state.products, productId),
         })),
+
+      clear: () => set(() => ({ products: [] })),
     }),
     {
       name: 'nlw-expert:cart',
